0847408a050d059be0bbad7bd9bee859b78e8999
abunaim5 <abunaim140@gmail.com>
2024-03-22 00:23:03 +0600

    added ref form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import StatefullForm from './components/StatefullForm/StatefullForm'
+import RefForm from './components/RefForm/RefForm'
+// import StatefullForm from './components/StatefullForm/StatefullForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <>
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
-      <StatefullForm></StatefullForm>
+      {/* <StatefullForm></StatefullForm> */}\
+      <RefForm></RefForm>
     </>
   )
 }
